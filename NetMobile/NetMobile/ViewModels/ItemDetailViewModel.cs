a653c3aae885a18f10088deafc4b0e5f31b71784
Vladimir Lukianov <lukianov@performIT.net>
2018-09-10 08:46:23 +0200

    Add project files.

---
+++ NetMobile/NetMobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using NetMobile.Models;
+
+namespace NetMobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
